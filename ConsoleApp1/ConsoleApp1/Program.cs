45df964c2acbca6d8350b40bbbb64e6783c8f215
3292810-10 <3292810-10>
2022-02-16 11:18:44 +0300

    lol

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        const int ECHO_PORT = 8080;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("--logged in--");
+
+
+
+            try
+            {
+                TcpClient eClient = new TcpClient("127.0.01", ECHO_PORT);
+
+                StreamReader readerStream = new StreamReader(eClient.GetStream());
+                NetworkStream writerStream = eClient.GetStream();
+
+                string dataToSend;
+
+                dataToSend = name;
+                dataToSend += "\r\n";
+
+                byte[] data = Encoding.ASCII.GetBytes(dataToSend);
+                writerStream.Write(data, 0, data.Length);
+
+                while (true)
+                {
+                    Console.Write(name + ":");
+
+                    dataToSend = Console.ReadLine();
+                    dataToSend += "\r\n";
+
+                    data = Encoding.ASCII.GetBytes(dataToSend);
+                    writerStream.Write(data, 0, data.Length);
+
+                    if (dataToSend.IndexOf("QUIT") > -1)
+                        break;
+
+                    string returnData;
+                    returnData = readerStream.ReadLine();
+                    Console.WriteLine("Server: " + returnData);
+                }
+                eClient.Close();
+            }
+            catch(Exception exp)
+            {
+                Console.WriteLine("Exception: " + exp.Message);
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        const int ECHO_PORT = 8080;
+        public static int nClients = 0;
+        static void Main(string[] args)
+        {
+            try
+            {
+                TcpListener clientListener = new TcpListener(ECHO_PORT);
+                clientListener.Start();
+                Console.WriteLine("Waiting for connections...");
+                while (nClients < 3)
+                {
+                    TcpClient client = clientListener.AcceptTcpClient();
+                    ClientHandler cHandler = new ClientHandler();
+                    cHandler.clientSocket = client;
+                    Thread clientThread = new Thread(new ThreadStart(cHandler.RunClient));
+                    clientThread.Start();
+                    nClients++;
+                }
+                clientListener.Stop();
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine("Exception: " + exp);
+            }
+        }
+
+    }
+}
+
